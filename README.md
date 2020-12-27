68f8b1362048c68a906a93afd6eee2bef96b61ee
Kaveri07 <73036737+Kaveri07@users.noreply.github.com>
2020-12-27 12:12:22 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Class-35
-Database Intro
+# asynchronousBallMovement
+Boiler plate for ball moving aysnchronously
---
+++ sketch.js
@@ -0,0 +1,56 @@
+var ball;
+var database;
+var position;
+
+function setup(){
+    createCanvas(500,500);
+
+    database = firebase.database();
+
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    var ballPosition = database.ref("ball/position");
+    ballPosition.on("value", readPosition)
+
+    //.on is like the listener of the database that checks for any changes in the database
+    //.set sets/changes values in the database
+    //.ref references vales in the database
+    //.val gets the actual value inside the database
+}
+
+function draw(){
+    background("white");
+
+    if(position !== undefined){
+        if(keyDown(LEFT_ARROW)){
+            writePosition(-1,0);
+        }
+        else if(keyDown(RIGHT_ARROW)){
+            writePosition(1,0);
+        }
+        else if(keyDown(UP_ARROW)){
+            writePosition(0,-1);
+        }
+        else if(keyDown(DOWN_ARROW)){
+            writePosition(0,+1);
+        }
+    }
+    
+    drawSprites();
+}
+
+function readPosition(data) {
+    position = data.val();
+
+    ball.x = position.x;
+    ball.y = position.y;
+}
+
+function writePosition(x,y) {
+    database.ref("ball/position").set({
+        "x": position.x + x,
+        "y": position.y + y
+    });
+    
+}
